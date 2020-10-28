3dd139940d2df74be6136e26fcc8d5a735f0d0c7
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-10-28 10:43:51 +0100

    added node api

---
+++ node-api/index.js
@@ -0,0 +1,11 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.register(require('fastify-cors'))
+
+app.get('/', (request, reply) => {
+    return reply.send({ msg: 'Node API running.' })
+})
+
+app.listen(6104)
